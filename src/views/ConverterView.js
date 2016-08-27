55e57eb09f0fa5acd8fb7efe4a63562c36505969
Jagadish Uppala <jagadishuppala@gmail.com>
2016-08-27 14:19:15 -0700

    + Added CurrencyPickerView component. * Update android index with iOS index require.
    Signed-off-by: Jagadish Uppala <jagadishuppala@gmail.com>

--- src/views/ConverterView.js
+++ src/views/ConverterView.js
@@ -5,41 +5,47 @@ import {
   TextInput,
   View,
   Alert,
-  UIExplorerBlock,
-  Picker
+  TouchableWithoutFeedback
 } from 'react-native';
 
+import CurrencyPickerView from './CurrencyPickerView';
 import rates from '../../data/rates.json';
 
 module.exports = React.createClass({
+
+  ratesList : [],
+
   getInitialState() {
-    console.log(rates);
+    this.ratesList = rates.map((item) => item.currency_code);
+
+    //TODO need to implement default from and to in pickers
+    let fromDefaultOption = '', toDefaultOption = '';
+    if(this.ratesList && this.ratesList.length >= 2) {
+      fromDefaultOption = this.ratesList[0];
+      toDefaultOption = this.ratesList[1];
+    }
+    let from = rates && rates[0] ? rates[0] : {};
+    let to = rates && rates[1] ? rates[1] :  {};
+    let toCurrencyValue = (to.rate || 1) / (from.rate || 1) + '';
     return ({
-      from:{
-        "currency_code": "EUR",
-        "rate": 0.8861,
-        "code": "EU",
-        "name": "European Union"
-      },
-      to:{
-        "currency_code": "INR",
-        "rate": 67.009,
-        "code": "RS",
-        "name": "India"
-      },
+      from,
+      to,
       fromCurrencyValue: '1',
       fromValue: '1',
-      toCurrencyValue: '75.6223',
-      toValue: '75.6223',
-      selected: 'key1',
-      color: 'red'
+      fromSelectedOption: from.currency_code || 'USD',//fromDefaultOption,
+      toCurrencyValue,
+      toValue: toCurrencyValue,
+      toSelectedOption: to.currency_code,//toDefaultOption,
+      selectedOption: ''
     })
   },
+
+  //Set currency type
   setCurrencyType(isFrom, type) {
     let ratesCollection = rates;
     let currencyModel = ratesCollection.find(item => item.currency_code === type);
     if(currencyModel === undefined)
-      return;
+    return;
     if(isFrom) {
       this.setState({from: currencyModel});
       let toValue = (this.state.to.rate / currencyModel.rate).toFixed(2);
@@ -52,37 +58,36 @@ module.exports = React.createClass({
       this.setState({ toCurrencyValue, toValue });
     }
   },
+
+  //Convert Currency
   convertCurrency(from, value) {
     //trim the value, just to ignore the alert message for empty spaces.
-
     let fromValue = value.trim();
     let toValue;
-    //Check for NaN to alert the user for invalid input.
-    if(Number.isNaN(Number.parseFloat(fromValue)) && value !== '' && value !== '.') {
+
+    //Regex check for valid input format for currency.
+    let isValidInput = /^-?(\d+\.?\d*)$|(\d*\.?\d+)$/.test(fromValue);// /(?:\d*\.)?\d+/
+    if(!isValidInput && fromValue.length !== 0) {
       Alert.alert(
-            'Invalid Currency input!',
-            `${value} is not a valid currency input. Please enter valid input in decimal format`
-          );
-          //Reset the value and that loads default view.
-          value = '';
+        'Invalid Currency input!',
+        `${value} is not a valid currency input. Please enter valid input in decimal format`
+      );
+      //Reset the value and that loads default view.
+      fromValue = '';
     }
     //Incase value in empty, set the state to default for the given from and to currency types.
-    switch (value) {
+    switch (fromValue) {
       case '':
-        fromValue = '';
-        toValue = '';
-        from = true;
-        break;
-      case '.':
-        toValue = ' ';
-        break;
+      fromValue = '';
+      toValue = '';
+      break;
       default:
-        let input = Number.parseFloat(fromValue);
-        toValue = from ?
-          this.state.to.rate*input/this.state.from.rate:
-          this.state.from.rate*input/this.state.to.rate;
-          toValue = toValue.toFixed(2);
-          break;
+      let input = Number.parseFloat(fromValue);
+      toValue = from ?
+      this.state.to.rate*input/this.state.from.rate:
+      this.state.from.rate*input/this.state.to.rate;
+      toValue = toValue.toFixed(2);
+      break;
     }
 
     if(from === false) {
@@ -90,94 +95,115 @@ module.exports = React.createClass({
     }
     this.setState({fromValue, toValue});
   },
-  changeMode() {
-    const newMode = this.state.mode === Picker.MODE_DIALOG
-        ? Picker.MODE_DROPDOWN
-        : Picker.MODE_DIALOG;
-    this.setState({mode: newMode});
-  },
-
-  onValueChange(key: string, value: string) {
-    const newState = {};
-    newState[key] = value;
-    this.setState(newState);
-  },
 
   render() {
     return (
       //Container for From and To Currency type and values.
       <View style={styles.container}>
-        <View style={styles.inputs}>
+        <View style={styles.flexRow}>
+          <View style={styles.container}>
+            <TouchableWithoutFeedback
+              style={{flex:1,flexDirection:'row'}}
+              onPress={() => this.refs.fromPicker.show()}
+              >
+              <View style={styles.boxViewStyle}>
+                <Text numberOfLines={1} style={styles.textStyle}>{this.state.from.currency_code}</Text></View>
+              </TouchableWithoutFeedback>
+              <CurrencyPickerView
+                ref={'fromPicker'}
+                selectedValue={this.state.fromSelectedOption}
+                options={this.ratesList}
+                onSubmit={(option) => {
+                  this.setState({
+                    fromSelectedOption: option
+                  });
+                  this.setCurrencyType(true, option);
+                }}
+                />
+            </View>
+            <View style={styles.container}>
+              <TextInput
+                style={styles.textInputStyle}
+                placeholder={this.state.fromCurrencyValue}
+                placeholderTextColor='#555'
+                keyboardType='numeric'
+                onChangeText={value => this.convertCurrency(true, value)}
+                value={this.state.fromValue}
+                />
+            </View>
+          </View>
 
-          {/*<Picker
-            style={styles.picker}
-            selectedValue={this.state.selected}
-            onValueChange={this.onValueChange.bind(this, 'selected')}
-            mode="dialog">
-            <Picker.Item label="hello" value="key0" />
-            <Picker.Item label="world" value="key1" />
-          </Picker>*/}
 
-          <TextInput
-            style={styles.input}
-            placeholder={this.state.from.currency_code}
-            placeholderTextColor="#555"
-            onChangeText={type => this.setCurrencyType(true, type)}
-            />
-          <TextInput
-            style={styles.input}
-            placeholder={this.state.fromCurrencyValue}
-            placeholderTextColor='#555'
-            keyboardType='numeric'
-            onChangeText={value => this.convertCurrency(true, value)}
-            value={this.state.fromValue}
-            />
+          <View style={styles.container}>
+            <View style={styles.flexRow}>
+              <View style={styles.container}>
+                <TouchableWithoutFeedback
+                  style={{flex:1,flexDirection:'row'}}
+                  onPress={() => this.refs.toPicker.show()}
+                  >
+                  <View style={styles.boxViewStyle}>
+                    <Text numberOfLines={1} style={styles.textStyle}>{this.state.to.currency_code}</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+                <CurrencyPickerView
+                  ref={'toPicker'}
+                  selectedValue={this.state.toSelectedOption}
+                  options={this.ratesList}
+                  onSubmit={(option) => {
+                    this.setState({
+                      fromSelectedOption: option
+                    });
+                    this.setCurrencyType(false, option);
+                  }}
+                  />
+              </View>
+              <View style={styles.container}>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder={this.state.toCurrencyValue}
+                  placeholderTextColor='#555'
+                  keyboardType='numeric'
+                  onChangeText={value => this.convertCurrency(false, value)}
+                  value={this.state.toValue}
+                  />
+              </View>
+            </View>
+          </View>
         </View>
-        <View style={styles.inputs}>
-          <TextInput
-            style={styles.input}
-            placeholder={this.state.to.currency_code}
-            placeholderTextColor='#555'
-            onChangeText={type => this.setCurrencyType(false, type)}
-            />
-          <TextInput
-            style={styles.input}
-            placeholder={this.state.toCurrencyValue}
-            placeholderTextColor='#555'
-            keyboardType='numeric'
-            onChangeText={value => this.convertCurrency(false, value)}
-            value={this.state.toValue}
-            />
-        </View>
-      </View>
-    )
-  }
-});
+      )
+    }
+  });
 
-//Basic styles for ConverterView screen.
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 20
-  },
-  inputs: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  input: {
-    flex: 1,
-    flexDirection: 'column',
-    height: 60,
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: 'black',
-    textAlign: 'center',
-    margin: 10,
-    padding: 10
-  },
-  picker: {
-    width: 100,
-  },
-  redFont: {
-    color: 'red'
-  }
-})
+  //Basic styles for ConverterView screen.
+  const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#049fd9'
+    },
+    flexColumn: {
+      flex:1
+    },
+    flexRow:{
+      flexDirection: 'row',
+      paddingTop: 20
+    },
+    boxViewStyle: {
+      flex:1, flexDirection:'row',
+      borderWidth:1, borderRadius: 8, borderColor: '#e8ebf1',
+      alignItems: 'center',
+      margin: 10
+    },
+    textStyle:{
+      textAlign:'left',
+      fontSize:25,
+      padding:5,
+      color:'#ffffff'
+    },
+    textInputStyle: {
+      height: 50,
+      textAlign: 'right',
+      borderWidth:1, borderRadius:8, borderColor: '#e8ebf1',
+      margin:10, padding: 10,
+      color:'#ffffff'
+    }
+  })
---
+++ src/views/CurrencyPickerView.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  Picker,
+  Dimensions
+} from 'react-native';
+
+const SCREEN = {
+  WIDTH: Dimensions.get('window').width,
+  HEIGHT: Dimensions.get('window').height
+};
+
+module.exports = React.createClass({
+  getInitialState() {
+    return {
+      isPickerShown: false,
+      selectedOption: this.props.selectedValue || this.props.options[0]
+    };
+  },
+
+  show() {
+    this.setState({
+      isPickerShown: true,
+    });
+  },
+
+  donePicker() {
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state.selectedOption);
+    }
+    this.setState({
+      isPickerShown: false,
+    });
+  },
+
+  dismissPicker() {
+    this.setState({
+      isPickerShown: false,
+      selectedOption: this.props.selectedValue || this.props.options[0]
+    });
+  },
+
+  onPickerValueChange(option) {
+    this.setState({
+      selectedOption: option,
+    });
+  },
+
+  renderPickerItem(option, index) {
+    const label = (this.props.labels) ? this.props.labels[index] : option;
+    return (
+      <Picker.Item
+        key={option}
+        value={option}
+        label={label}
+      />
+    );
+  },
+
+  render() {
+
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={this.state.isPickerShown}
+      >
+        <View style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center'
+          }}
+          >
+          <View style={styles.modalContainer}>
+            <View style={styles.pickerContainer}>
+              <TouchableOpacity onPress={this.dismissPicker}>
+                <Text style={styles.pickerBtnStyle}>
+                  Close
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={this.donePicker}>
+                <Text style={styles.pickerBtnStyle}>
+                  Done
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.mainBox}>
+              <Picker
+                ref={'picker'}
+                style={{width: SCREEN.WIDTH}}
+                selectedValue={this.state.selectedOption}
+                onValueChange={(option) => {
+                  this.onPickerValueChange(option);
+                }}
+              >
+                {this.props.options.map((option, index) => this.renderPickerItem(option, index))}
+              </Picker>
+            </View>
+
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  basicContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  modalContainer: {
+    width: SCREEN.WIDTH,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  pickerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: SCREEN.WIDTH,
+    padding: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#64bbe3',
+    justifyContent: 'space-between'
+  },
+  pickerBtnStyle: {
+    fontSize: 20,
+    color: '#049fd9'
+  }
+});
